4bf9fda96ec65ebcbb2b04d8652d9b0e4a0cbadf
unknown <N7172@K310S11.dune.edu>
2023-10-02 14:37:20 +0200

    first commit

---
+++ src/tsikl2/Tsikl2.java
@@ -0,0 +1,36 @@
+
+package tsikl2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author N7172
+ */
+public class Tsikl2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("введите число");
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int m;
+        if (N==2)
+            System.out.println("число простое");
+            
+      for( m=2 ; m< N ; m++)
+      {
+          
+        if (N% m==0){
+            System.out.println("число не простое");
+            break;
+        } else if(m==N-1){
+             System.out.println("число простое");
+             break;
+      }
+    }
+    }
+}
